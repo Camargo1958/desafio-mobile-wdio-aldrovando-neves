e25b61be6b280418bd777513d733c1b600d620c3
Camargo1958 <aldrov_camargo@hotmail.com>
2025-10-21 21:01:09 -0300

    Novos testes para formScreen

--- screens/android/forms.android.screen.js
+++ screens/android/forms.android.screen.js
@@ -1,14 +1,22 @@
 export default class FormsAndroidScreen {
 
+    get formFrame() { return $('//android.widget.ScrollView[@content-desc="Forms-screen"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]') }
     get lblTitle() { return $('//android.widget.TextView[@text="Form components"]') }
     get inputField() { return $('//android.widget.EditText[@content-desc="text-input"]') }
     get lblYouHaveTyped() { return $('//android.widget.TextView[contains(@text, "You have typed:")]') }
     get inputTextResult() { return $('~input-text-result') }
     get toggleSwitch() { return $('//android.widget.Switch[@content-desc="switch"]') }
     get switchStatusText() { return $('~switch-text') }
-    get dropdownList() { return $('//android.widget.Spinner[@content-desc="Dropdown"]') }
-    get btnActive() { return $('//android.widget.Button[@text="Active"]') }
-    get btnInactive() { return $('//android.widget.Button[@text="Inactive"]') }
+    get dropdownListOpenBtn() { return $('//android.widget.TextView[@text="󰅀"]') }
+    get dropDownListSelection() { return $('//android.widget.EditText[@resource-id="text_input"]') }
+    get btnActive() { return $('//android.widget.TextView[@text="Active"]') }
+    get btnInactive() { return $('//android.widget.TextView[@text="Inactive"]') }
+    get alertModalTitle() { return $('[id="android:id/alertTitle"]') }
+    get alertModalMessage() { return $('//android.widget.TextView[@resource-id="android:id/message"]') }
+    get lblButtons() { return $('//android.widget.TextView[@text="Buttons"]') }
+    get alertModalBtnOk() { return $('[text="OK"]') }
+    get alertModalBtnAskMeLater() { return $('[text="ASK ME LATER"]') }
+    get alertModalBtnCancel() { return $('[text="CANCEL"]') }
 
     async getTitleText() {
         return await this.lblTitle.getText()
@@ -42,15 +50,19 @@ export default class FormsAndroidScreen {
     }
 
     async selectDropdownOption(optionText) {
-        await this.dropdownList.waitForDisplayed({ timeout: 5000 })
-        await this.dropdownList.click()
-        const option = $(`//android.widget.TextView[@text="${optionText}"]`)
+        await this.dropdownListOpenBtn.waitForDisplayed({ timeout: 5000 })
+        await this.dropdownListOpenBtn.click()
+        const option = $(`[text="${optionText}"]`)
         await option.waitForDisplayed({ timeout: 5000 })
         await option.click()
     }
 
     async getSelectedDropdownText() {
-        return await this.dropdownList.getText()
+        return await this.dropDownListSelection.getText()
+    }
+
+    async scrollToLblButtons() {
+        await this.lblButtons.scrollIntoView()
     }
 
     async clickBtnActive() {
@@ -63,8 +75,37 @@ export default class FormsAndroidScreen {
         await this.btnInactive.click()
     }
 
+    async isDisplayedAlertModal() {
+        return await this.alertModalTitle.isDisplayed()
+    }
+
+    async getAlertModalTitleText() {
+        await this.alertModalTitle.waitForDisplayed({ timeout: 5000 })
+        return await this.alertModalTitle.getText()
+    }
+
+    async getAlertModalMessageText() {
+        await this.alertModalMessage.waitForDisplayed({ timeout: 5000 })
+        return await this.alertModalMessage.getText()
+    }
+
+    async clickAlertModalBtnOk() {
+        await this.alertModalBtnOk.waitForDisplayed({ timeout: 5000 })
+        await this.alertModalBtnOk.click()
+    }
+
+    async clickAlertModalBtnAskMeLater() {
+        await this.alertModalBtnAskMeLater.waitForDisplayed({ timeout: 5000 })
+        await this.alertModalBtnAskMeLater.click()
+    }
+
+    async clickAlertModalBtnCancel() {
+        await this.alertModalBtnCancel.waitForDisplayed({ timeout: 5000 })
+        await this.alertModalBtnCancel.click()
+    }
+
     async waitForIsShown(isShown = true) {
-        await this.lblTitle.waitForDisplayed({
+        await this.formFrame.waitForDisplayed({
             timeout: 10000,
             reverse: !isShown
         })
--- tests/forms/validateFormsScreen.js
+++ tests/forms/validateFormsScreen.js
@@ -4,18 +4,21 @@ import { allureReportsHelpers, assertJS, homeScreen, formsScreen } from "../../c
 import { describeName } from "../../data/describe.data"
 import { feature } from "../../data/feature.data"
 import { severity } from "../../data/severity.data"
-import { browser } from '@wdio/globals'
 
 describe(describeName.app1, () => {
 
     beforeEach(async () => {
         await homeScreen.waitForIsShown(true)
+        await homeScreen.clickBtnForms()
+    })
+
+    afterEach(async () => {
+        await homeScreen.clickBtnHome()
     })
 
     it(`[APP01-000008] deve visualizar forms screen e validar título`, async () => {
         await allureReportsHelpers.addAllureMobile(930, severity.normal, feature.forms)
 
-        await homeScreen.clickBtnForms()
         await formsScreen.waitForIsShown(true)
         assertJS.isTrue(await formsScreen.getTitleText() === "Form components")
     })
@@ -23,9 +26,7 @@ describe(describeName.app1, () => {
     it(`[APP01-000009] deve digitar um texto no input field e verificar saída`, async () => {
         await allureReportsHelpers.addAllureMobile(931, severity.normal, feature.forms)
 
-        await homeScreen.clickBtnForms()
         await formsScreen.waitForIsShown(true)
-
         const testText = "Hello World"
         await formsScreen.setInputFieldText(testText)
         const outputText = await formsScreen.getInputTextResultText()
@@ -34,9 +35,6 @@ describe(describeName.app1, () => {
 
     it(`[APP01-000010] deve alternar o switch e verificar o status`, async () => {
         await allureReportsHelpers.addAllureMobile(932, severity.normal, feature.forms)
-        
-        await homeScreen.clickBtnForms()
-        await formsScreen.waitForIsShown(true)
 
         const initialStatus = await formsScreen.getSwitchStatusText()
         await formsScreen.toggleSwitchOnOff()
@@ -47,24 +45,54 @@ describe(describeName.app1, () => {
     it(`[APP01-000011] deve selecionar uma opção do dropdown e verificar a seleção`, async () => {
         await allureReportsHelpers.addAllureMobile(933, severity.normal, feature.forms)
 
-        await homeScreen.clickBtnForms()
-        await formsScreen.waitForIsShown(true)
-
         const optionToSelect = "Appium is awesome"
         await formsScreen.selectDropdownOption(optionToSelect)
         const selectedOption = await formsScreen.getSelectedDropdownText()
         assertJS.isTrue(selectedOption === optionToSelect)
     })
 
-    it(`[APP01-000012] deve clicar no botão Active e Inactive`, async () => {
+    it(`[APP01-000012] deve clicar no botão "Active" e modal ser apresentado`, async () => {
         await allureReportsHelpers.addAllureMobile(934, severity.normal, feature.forms)
 
-        await homeScreen.clickBtnForms()
-        await formsScreen.waitForIsShown(true)
-
+        await formsScreen.scrollToLblButtons()
         await formsScreen.clickBtnActive()
-        // Aqui você pode adicionar uma verificação específica para o estado "Active" se houver feedback visual     
+        assertJS.isTrue(await formsScreen.alertModalTitle.getText() === "This button is")
+        assertJS.isTrue(await formsScreen.alertModalMessage.getText() === "This button is active")
+        await formsScreen.alertModalBtnOk.click()
+    })
+
+    it(`[APP01-000013] deve fechar o modal ao clicar no botão "OK"`, async () => {
+        await allureReportsHelpers.addAllureMobile(934, severity.normal, feature.forms)
+
+        await formsScreen.scrollToLblButtons()
+        await formsScreen.clickBtnActive()
+        await formsScreen.alertModalBtnOk.click()
+        assertJS.isFalse(await formsScreen.isDisplayedAlertModal())
+    })
+
+    it(`[APP01-000014] deve fechar o modal ao clicar no botão "ASK ME LATER"`, async () => {
+        await allureReportsHelpers.addAllureMobile(934, severity.normal, feature.forms)
+
+        await formsScreen.scrollToLblButtons()
+        await formsScreen.clickBtnActive()
+        await formsScreen.alertModalBtnAskMeLater.click()
+        assertJS.isFalse(await formsScreen.isDisplayedAlertModal())
+    })
+
+    it(`[APP01-000015] deve fechar o modal ao clicar no botão "ASK ME LATER"`, async () => {
+        await allureReportsHelpers.addAllureMobile(934, severity.normal, feature.forms)
+
+        await formsScreen.scrollToLblButtons()
+        await formsScreen.clickBtnActive()
+        await formsScreen.alertModalBtnCancel.click()
+        assertJS.isFalse(await formsScreen.isDisplayedAlertModal())
+    })
+
+    it(`[APP01-000016] deve clicar no botão Inactive e modal nãão deve ser apresentado`, async () => {
+        await allureReportsHelpers.addAllureMobile(934, severity.normal, feature.forms)
+
+        await formsScreen.scrollToLblButtons()
         await formsScreen.clickBtnInactive()
-        // Aqui você pode adicionar uma verificação específica para o estado "Inactive" se houver feedback visual     
+        assertJS.isFalse(await formsScreen.isDisplayedAlertModal())
     })
 })
